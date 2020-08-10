bf25075ee2887cd88b941764362755c26576b6c4
Amiri <51239320+AmiriMc@users.noreply.github.com>
2020-08-10 12:03:55 -0700

    Initial version.
    Initial version for reference purposes.

--- etl.py
+++ etl.py
@@ -24,24 +24,48 @@ def create_spark_session():
 def process_song_data(spark, input_data, output_data):
     '''
     This function reads the data from S3, processes the song data using Spark, and writes the processed data back to S3.
+
+    Parameters:
+    - spark: The spark session
+    - input_data: The S3 path location up to, but not including `song_data`
+    - output_data: The S3 bucket where the new dimensional tables will be written to
     '''
     # get filepath to song data file
-    song_data = "s3://udacity-dend/song_data/" + input_data
+    start = time.time()
+    song_data =  input_data + 'song_data/*/*/*/*.json'
+    end = time.time()
+    print('get song filepath runtime (s):', end-start)
     
     # read song data file
-    df = spark.read.load(song_data)
+    start = time.time()
+    df = spark.read.json(song_data) # may not need this new schema
+    end = time.time()
+    print('read song data file runtime (s):', end-start)
 
     # extract columns to create songs table
-    songs_table = 
-    
+    start = time.time()
+    songs_table = df.select("song_id", "title", "artist_id", "year", "duration").distinct()
+    end = time.time()
+    print('extract columns to create songs table runtime (s):', end-start)
+
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    start = time.time()
+    songs_table_parquet = df.select("song_id", "title", "artist_id", "year", "duration", col("artist_name").alias("artist")).distinct()
+    songs_table_parquet.write.mode('overwrite').partitionBy("year", "artist_id").parquet(output_data + "songs/")
+    end = time.time()
+    print('write songs table to parquet files runtime (s):', end-start)
 
     # extract columns to create artists table
-    artists_table = 
-    
+    start = time.time()
+    artists_table = df.select("artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude").distinct()
+    end = time.time()
+    print('extract columns to create artists table runtime (s):', end-start)
+
     # write artists table to parquet files
-    artists_table
+    start = time.time()
+    artists_table_parquet = artists_table.write.mode('overwrite').parquet(output_data + "artists/")
+    end = time.time()
+    print('write artists table to parquet files runtime (s):', end-start)
 
 
 def process_log_data(spark, input_data, output_data):
@@ -88,6 +112,10 @@ def process_log_data(spark, input_data, output_data):
 
 
 def main():
+    '''
+    Create Spark session, provide paths to input data and output data, load songs/log data
+    and create parquet tables (columnar format) with star schema DB.
+    '''
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = "s3a://dend-amiri-spark/output_data/"
